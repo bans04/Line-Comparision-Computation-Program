1773a439f718d7d4772454e62d1b53e821338608
bans04 <bansodednyanadeo@gmail.com>
2022-06-12 10:56:07 +0530

    Added:-> Compare length of two line using .equeals() method

--- src/com/linecomparision/LineComparision.java
+++ src/com/linecomparision/LineComparision.java
@@ -4,6 +4,8 @@ package com.linecomparision;
  * -> Print welcome message
  * -> Calculate Length of Line
  * 
+ * @Dnyandeo Bansode
+ * 
  * */
 
 public class LineComparision {
@@ -15,7 +17,21 @@ public class LineComparision {
 		line1.x2 = 9;
 		line1.y1 = 3;
 		line1.y2 = 8;
+		Double line1Length = line1.lineLength();
+		System.out.println("Length of Line1:-> " + line1Length);
 		
-		System.out.println("Length of Line:-> " + line1.lineLength());
+		Line line2 = new Line();
+		line2.x1 = 2;
+		line2.x2 = 9;
+		line2.y1 = 3;
+		line2.y2 = 8;
+		double line2Length = line2.lineLength();
+		System.out.println("Length of line2:-> " + line2Length);
+		
+		if(line1Length.equals(line2Length)) {
+			System.out.println("line1 and line2 is equals..|");
+		}else {
+			System.out.println("line1 and line2 is not equals..|");
+		}
 	}
 }
