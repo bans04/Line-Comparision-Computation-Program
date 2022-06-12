45335f011030fa989c7ab8dd3931e347e38b48c4
bans04 <bansodednyanadeo@gmail.com>
2022-06-12 09:55:32 +0530

    Added:-> Print Welcome message

---
+++ src/com/linecomparision/LineComparision.java
@@ -0,0 +1,8 @@
+package com.linecomparision;
+
+public class LineComparision {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparision Computation Program");
+	}
+}
